a3f09b0d93880f755f93127af4e024e95f453993
brandenisnotverycool <brandenhuang69@gmail.com>
2019-06-25 15:56:04 -0700

    ok cool

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys, pygame, random, pandas as pd
+from ship import Ship
+from asteroid import Asteroid
+from pygame.locals import *
+
+pygame.init()
+screen_info=pygame.display.Info()
+size=(width,height)=(int(screen_info.current_w*0,5), int(screen_info.current_h*0,5))
+window=pygame.display.set_mode(size)
+clock=pygame.time.Clock()
+color=(40,0,40)
+window.fill(color)
+df = pd.read_csv('game_info.csv')
+asteroids=pygame.sprite.Group()
+numLevels=df['LevelNum'].max()
+
+level=df['LevelNum'].min()
+
+levelData=df.iloc[level]
+asteroidCount=levelData['AsteroidCount']
+player = Ship((levelData['PlayerX'], levelData['PlayerY']))
+
+def init():
+    global asteroidCount,asteroids,levelData
+    levelData = df.iloc[level]
+    player.reset((levelData['PlayerX'], levelData['PlayerY']))
+    asteroids.empty()
+    asteroidCount=levelData['AsteroidCount']
+    for i in range(asteroidCount):
+        asteroids.add(Asteroid((random.randint(50, width-50),
+                                random.randint(50,height-50), random.randint(15,60))))
+
+def win():
+    font=pygame.font.SysFont(None,70)
+    text=font.render("Misson Success", True,(255,0,0))
+    text_rect=text.get_rect()
+    text_rect.center=(width/2,height/2)
+    while True:
+        window.fill(color)
+        window.blit(text,text_rect)
+        pygame.display.flip()
+
+
+
+
+
